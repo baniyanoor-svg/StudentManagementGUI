365adf6e90ce1a274039d5962c3dfb96c4849a56
baniyanoor-svg <baniyanoor@gmail.com>
2026-05-13 17:44:47 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student {
+
+    int id;
+    String name;
+    String course;
+
+    Student(int id, String name, String course) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+    }
+}
